293d76fcaa4f152cd5e3c994293e17ab5681f9dc
Erica Rivera <ericar@fb.com>
2017-07-03 12:23:43 -0700

    "simple chat app work"

---
+++ app/src/main/java/com/codepath/simplechat/ChatActivity.java
@@ -0,0 +1,13 @@
+package com.codepath.simplechat;
+
+import android.service.autofill.SaveCallback;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class ChatActivity extends AppCompatActivity {
+
+    static final int MAX_CHAT_MESSAGES_TO_SHOW = 50;
